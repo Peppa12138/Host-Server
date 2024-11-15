6512b3cd8ef8cd81411a0045879123a293f6e270
gyf <2528357407@qq.com>
2024-11-15 20:51:48 +0800

    删除了一些无用的代码

--- routes/sensor.js
+++ routes/sensor.js
@@ -1,38 +1,3 @@
-// const express = require('express');
-// const db = require('../db');
-// const { publishControl } = require('../mqttClient');
-// const router = express.Router();
-
-// router.get('/data', (req, res) => {
-//     db.query('SELECT * FROM sensor_data', (err, results) => {
-//         if (err) throw err;
-//         res.json(results);
-//     });
-// });
-
-// router.get('/logs', (req, res) => {
-//     db.query('SELECT * FROM control_logs', (err, results) => {
-//         if (err) throw err;
-//         res.json(results);
-//     });
-// });
-
-// router.post('/control', (req, res) => {
-//     const { action } = req.body;
-
-//     if (!action) {
-//         return res.status(400).send({ error: '需要传入 action' });
-//     }
-
-//     publishControl(action);
-
-//     db.query('SELECT * FROM sensor_data', (err, results) => {
-//         if (err) throw err;
-//         console.log(results);
-//         res.json(results);
-//     });
-// });
-// module.exports = router;
 const express = require("express");
 const db = require("../db");
 const { publishControl } = require("../mqttClient");
